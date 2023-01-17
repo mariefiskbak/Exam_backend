fcb826d243916444b6e380e2477750ff67755d76
Marie Fiskbæk <marie.fiskbak@gmail.com>
2023-01-17 10:38:43 +0100

    test

--- src/main/java/entities/Role.java
+++ src/main/java/entities/Role.java
@@ -27,7 +27,8 @@ public class Role implements Serializable {
     @NotNull
     @Column(name = "role_name", length = 20)
     private String roleName;
-    
+
+
     @ManyToMany(mappedBy = "roleList")
     private List<User> userList;
 
